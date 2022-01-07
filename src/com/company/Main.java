a59ef5c293d740dc621e8e0ebe49681bfbac83cc
dm210772 <dm210772@reigate.ac.uk>
2022-01-07 13:28:40 +0000

    Started implementing classes (still unfinished)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import com.company.fileHandling.*;
 import java.util.Scanner;
 
 public class Main {
@@ -19,55 +16,9 @@ public class Main {
         return input.nextLine();
     }
 
-    public static void createFile() {
-        try {
-            if (books.createNewFile()) {
-                System.out.println("File created: " + books.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
-    public static void writeToFile(String userInput) {
-        try {
-            FileWriter myWriter = new FileWriter(books.getName(), true);
-            myWriter.write(userInput + "\n");
-            myWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
-    public static void readFile() {
-        try {
-            Scanner myReader = new Scanner(books);
-            while (myReader.hasNextLine()) {
-                String[] data = myReader.nextLine().split(",");
-                System.out.println(Arrays.toString(data));
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
-    public static void deleteFile() {
-        if (books.delete()) {
-            System.out.println("Deleted the file: " + books.getName());
-        } else {
-            System.out.println("Failed to delete the file.");
-        }
-    }
-
     public static void main(String[] args) {
         // write your code here
-        createFile();
+        fileHandling.createFile();
         boolean again = true;
         while (again) {
             String userChoice = getInput("Would you like to write to, read, or delete the file?");
---
+++ src/com/company/loginSystem.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class loginSystem {
+
+    static File accounts = new File("accounts.txt");
+
+    public static void registerLogIn(String username, String account) {
+        try {
+            FileWriter myWriter = new FileWriter(accounts.getName(), true);
+            myWriter.write(username + "," + account + "\n");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static String getInput(String prompt) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(prompt);
+        return input.nextLine();
+    }
+
+    public static void verifyLogIn(String username, String password) {
+        try {
+            Scanner myReader = new Scanner(accounts);
+            while (myReader.hasNextLine()) {
+                String[] data = myReader.nextLine().split(",");
+                if (data[0].equals(username)) {
+                    if (data[1].equals(password)) {
+                        System.out.println("Password correct. Logging in...");
+                        System.out.println("Log in complete.");
+                        break;
+                    } else {
+                        System.out.println("Password incorrect.");
+                    }
+                }
+            }
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String choice = getInput("Would you like to log in or register an account?");
+        if (choice.equalsIgnoreCase("log in")) {
+            verifyLogIn(getInput("Username: "), getInput("Password: "));
+        } else if (choice.equalsIgnoreCase("register")) {
+            registerLogIn(getInput("Username: "), getInput("Password: "));
+        } else if (choice.equalsIgnoreCase("close")) {
+            System.exit(0);
+        }
+    }
+}
